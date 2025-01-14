99b7d8e98514c7d8ad121fb410c31a7f59080ca0
Joao Pedro Gonzatti <joaopedro.gonzatti@hotmail.com>
2025-01-14 16:32:55 -0300

    Adiciona arquivos faltantes

--- screen-match.php
+++ screen-match.php
@@ -1,35 +1,50 @@
 <?php
-echo "Bem-vindo(a) ao screen match!
-";
+
+echo "Bem-vindo(a) ao screen match!\n";
+
 $nomeFilme = "Top Gun - Maverick";
+
 $anoLancamento = 2022;
-$notaFilme =  8.8;
-$incluidoNoPlano = true;
+
 $quantidadeDeNotas = $argc - 1;
 $notas = [];
+
 for ($contador = 1; $contador < $argc; $contador++) {
-    $notas[] = (float) $agrv[$contador];
+    $notas[] = (float) $argv[$contador];
 }
-$somaDeNotas = 0;
-foreach ($notas as $nota) {
-    $somaDeNotas += $nota;
-}
-$notaFilme = $somaDeNotas / $quantidadeDeNotas;
+
+$notaFilme = array_sum($notas) / $quantidadeDeNotas;
 $planoPrime = true;
-$incluidoNoPlano = $planoPrime ||  $anoLancamento < 2020;
-echo "Nome do filme:" . $nomeFilme . "\n";
-echo "Nota do filme: $notaFilme\n"; 
+
+$incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
+
+echo "Nome do filme: " . $nomeFilme . "\n";
+echo "Nota do filme: $notaFilme\n";
 echo "Ano de lançamento: $anoLancamento\n";
-if ($anoLancamento > 2022) {
-    echo "Esse filme é um lançamento";
-} elseif($anoLancamento > 2020 && $anoLancamento <= 2022) {
-    echo "Esse filme ainda é novo";
+
+if ($ano > 2022) {
+    echo "Esse filme é um lançamento\n";
+} elseif($ano > 2020 && $ano <= 2022) {
+    echo "Esse filme ainda é novo\n";
 } else {
-    echo "Esse filme não é um lançamento";
+    echo "Esse filme não é um lançamento\n";
 }
+
+$genero = match ($nomeFilme) {
+    "Top Gun - Maverick" => "ação",
+    "Thor: Ragnarok" => "super-herói",
+    "Se beber não case" => "comédia",
+    default => "gênero desconhecido",
+};
+
+echo "O gênero do filme é: $genero\n";
+
 $filme = [
     "nome" => "Thor: Ragnarok",
-    "ano" =>  2021,
+    "ano" => 2021,
     "nota" => 7.8,
     "genero" => "super-herói",
 ];
+
+
+echo $filme["ano"];
