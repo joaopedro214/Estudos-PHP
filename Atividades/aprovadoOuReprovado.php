e71e9676a30af36bf1a57d51409b7c3ff7de1b94
Joao Pedro Gonzatti <joaopedro.gonzatti@hotmail.com>
2025-01-14 11:36:39 -0300

    Commit de ajuste de diretório

--- Atividades/aprovadoOuReprovado.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$notas = [4, 6, 7, 3, 5, 9, 10, 8];
-
-foreach ($notas as $nota){
-    $resultado = $nota > 6 ? "aprovado" : "reprovado";
-
-    echo "Essa(a) aluno(a) foi $resultado com a nota $nota \n";
-}
--- Atividades/converteMetros.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo "Converter metros para centímetros.\nEscolha a quantia em metros que será convertida: ";
-$qtdMetros = trim(fgets(STDIN));
-
-$qtdCentimetros = $qtdMetros * 100;
-
-echo "A quantidade em centimetros é: $qtdCentimetros";
