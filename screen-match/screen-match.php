c05dce9c1e0994cbcd41240c7ec3e6935ec557cf
Joao Pedro Gonzatti <joaopedro.gonzatti@hotmail.com>
2025-01-15 22:32:52 -0300

    Separar functions em outro arquivo e constante __DIR__

--- screen-match/screen-match.php
+++ screen-match/screen-match.php
@@ -1,19 +1,7 @@
 <?php
-// VOID=vazio informamos com : void que não há retorno
-function exibeMensagemLancamento(int $ano): void {
-    if ($ano > 2022) {
-        echo "Esse filme é um lançamento\n";
-    } elseif($ano > 2020 && $ano <= 2022) {
-        echo "Esse filme ainda é novo\n";
-    } else {
-        echo "Esse filme não é um lançamento\n";
-    }
-}
 
-function incluidoNoPlano(bool $planoPrime, int $anoLancamento): bool {
-    return $planoPrime || $anoLancamento < 2020;
-    
-}
+// é uma constante que retorna o diretório do arquivo onde é usada. Útil quando precisar de um caminho absoluto.
+require __DIR__ . "funcoes.php";
 
 echo "Bem-vindo(a) ao screen match!\n";
 
