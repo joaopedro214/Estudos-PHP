87d418c1ca057c1b9138a57d85473e6f9ff8c46d
Joao Pedro Gonzatti <joaopedro.gonzatti@hotmail.com>
2025-01-27 19:33:08 -0300

    Corrige erro de sintaxe

--- public/exporta-arquivo.php
+++ public/exporta-arquivo.php
@@ -7,7 +7,7 @@ $filme = [
     'nome' => $_POST['nome'],
     'anoLancamento' => $_POST['ano'],
     'nota' => $_POST['nota'],
-    'geenero' => $_POST['genero'],
+    'genero' => $_POST['genero'],
 ];
 
 file_put_contents('filme.json', json_encode($filme));
